e2bb2282d898e2d4159c531fbf1639fd8411e818
Tatyana404 <kot.kate2020@gmail.com>
2021-03-14 22:32:52 +0200

    Refactor regular expression

--- utils/validationSchemas.js
+++ utils/validationSchemas.js
@@ -1,9 +1,8 @@
 const yup = require('yup')
 
 module.exports.VALID_TODO = yup.object({
-  body: yup
-    .string()
-    .matches(/^[\w][\w\W]{2,}$/, 'Сannot create an empty list')
+  body: Yup.string()
+    .matches(/^.{2,}$/, 'Сannot create an empty list')
     .trim()
     .required(),
   isDone: yup.boolean()
